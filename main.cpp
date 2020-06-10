dfe76ef6351ed107491ba48c4e12cfba62fa9423
miszazmisza <62642076+miszazmisza@users.noreply.github.com>
2020-06-10 10:32:20 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdio>
+#include <fstream>
+using namespace std;
+int main()
+{
+    int t[100]];
+    int a, i;
+    ifstream liczby("1 3 2 4 2 2 3 5");
+	t[0]==1;
+	max = t[0];
+	
+	for (int i=0; i<500; i++){
+		
+    	cout << t[i];
+    	return 0;
+	
+		if(t[i] > max){
+		max=t[i]
+		}
+	
+	}
+	
+	{ "max" : 4, "losowa_ocena": 3}
+}

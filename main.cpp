c561a6aa61d7c747dd84d6bfdcca3eb777907489
miszazmisza <62642076+miszazmisza@users.noreply.github.com>
2020-10-29 11:29:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,36 +16,34 @@ int main(int argc, char** argv)
  ifstream file1;
  ifstream file2;
  ofstream fileOut; //plik wynikowy do zapisu
- char* endptr; // to jest wskaŸnik na miejsce, gdzie przy konwersji z systemu czworkowego wyst¹pi b³¹d - nieu¿ywana zmienna
+ char* endptr; // to jest wskaÅºnik na miejsce, gdzie przy konwersji z systemu czworkowego wystÄ…pi bÅ‚Ä…d - nieuÅ¼ywana zmienna
  long liczbap,liczbapo,liczbapop,rekord1,rekord2,rekord3; //liczba dziesietna, minimum z pliku
- string liczba1,liczba2,liczba3,liczba4,liczba5,liczba6,liczbal,liczball,liczballl, max, max2,max3; //dwie liczby w systemie czworkowym wczytujemy jako "string" bo mog¹ byæ d³u¿sze od long long
+ string liczba1,liczba2,liczba3,liczba4,liczba5,liczba6,liczbal,liczball,liczballl, max, max2,max3; //dwie liczby w systemie czworkowym wczytujemy jako "string" bo mogÄ… byÄ‡ dÅ‚uÅ¼sze od long long
  file.open("dane_systemy1.txt");
  file1.open("dane_systemy2.txt");
  file2.open("dane_systemy3.txt"); //otworz plik liczb
  fileOut.open("wyjscie.txt"); //otworz plik do wynikow
- bool pierwszy = true;  //do algorytmu MINIMUM - zak³adamy, ¿e pierwsza liczba jest najmniejsza z dotychczas przeczytanych
+ bool pierwszy = true;  //do algorytmu MINIMUM - zakÅ‚adamy, Å¼e pierwsza liczba jest najmniejsza z dotychczas przeczytanych
  
  if(file.good())  //sprawdzenie czy plik istnieje
      while(!file.eof())        //petla wykonuje sie az program dojedzie do konca pliku
      {
-               file>>liczba1 >> liczba2;   // wczyta dwie liczby z pliku (w wierszu) do 2 stringów
+               file>>liczba1 >> liczba2;   // wczyta dwie liczby z pliku (w wierszu) do 2 stringÃ³w
                liczbal =  strtol(liczba2.c_str(), &endptr, 4);
                file1>>liczba3 >> liczba4; 
                liczball =  strtol(liczba4.c_str(), &endptr, 4);
                file2>>liczba5 >> liczba6;  
                liczballl =  strtol(liczba6.c_str(), &endptr, 4);
-               //^konwersja z systemu czworkowego do dziesietnego, string (C++) trzeba zamieniæ na char * (C) funkcja .c_str()
-               //zmienna endptr wskazuje na znak w stringu, który nie pasuje do systemu czwórkowego - tu niesprawdzone, bo zak³adamy, ¿e dane s¹ poprawne!
-               if(pierwszy) //jeœli to pierwsza liczba z pliku - bêdzie nowym MINIMUM
+               //^konwersja z systemu czworkowego do dziesietnego, string (C++) trzeba zamieniÄ‡ na char * (C) funkcja .c_str()
+               //zmienna endptr wskazuje na znak w stringu, ktÃ³ry nie pasuje do systemu czwÃ³rkowego - tu niesprawdzone, bo zakÅ‚adamy, Å¼e dane sÄ… poprawne!
+               if(pierwszy) //jeÅ›li to pierwsza liczba z pliku - bÄ™dzie nowym MINIMUM
                {
                             max = liczbap;
-                            pierwszy = false;
                             max2 = liczbapo;
-                            pierwszy = false;
                             max3 = liczbapop;
                             pierwszy = false;
                }
-               else{ //jeœli to kolejna liczba z pliku...
+               else{ //jeÅ›li to kolejna liczba z pliku...
                   bool rekord1 = false;
 				  if(liczbal > max){//... i jest mniejsza od dotychczasowego minimum...
                        max = liczbap; // ... to nadpisz min
@@ -66,7 +64,7 @@ int main(int argc, char** argv)
                 	}
 				}
      }//eof() koniec pliku
- fileOut<< "maksimum to: " << max <<"\nliczba rekordow to: "<<liczbaRekordow<<endl; //zapisanie najmniejszej liczby z pliku czworkowego do pliku wynikowego
+ fileOut<<liczbaRekordow<<endl; //zapisanie najmniejszej liczby z pliku czworkowego do pliku wynikowego
  file.close();
  file1.close();
  file2.close();
